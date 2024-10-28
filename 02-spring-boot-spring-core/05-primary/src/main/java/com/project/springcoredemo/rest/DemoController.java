3c4db791b46e8e13ce2bfc40b61c1410668088f5
Anishi Mishra <55035814+anishimishra@users.noreply.github.com>
2024-10-28 19:39:59 +0530

    Add files via upload

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/project/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.project.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.springcoredemo.common.Coach;
+
+@RestController
+public class DemoController {
+	// define a private field for the dependency
+	private Coach myCoach;
+
+	@Autowired
+	public DemoController(Coach theCoach) {
+		myCoach = theCoach;
+	}
+
+	@GetMapping("/dailyworkout")
+	public String getDailyWorkout() {
+		return myCoach.getDailyWorkout();
+	}
+
+}
---
+++ 02-spring-boot-spring-core/06-primary-qualifier/src/main/java/com/project/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,16 @@
+package com.project.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a hard 5k";
+	}
+
+}
