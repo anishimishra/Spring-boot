4f11ee289e4044c12105a0c1d530458c7e67765f
Anishi Mishra <55035814+anishimishra@users.noreply.github.com>
2024-10-28 19:49:44 +0530

    Add files via upload

---
+++ 06-spring-boot-spring-mvc/04-thymeleafdemo-helloworld-form-GetPostMapping/src/main/java/com/project/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.project.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloWorldController {
+	// need a controller method to show initial HTML form
+	@GetMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+
+	// need a controller method to read form data
+	// and add data to the model
+	@PostMapping("/processFormVersionTwo")
+	public String letsShoutDude(@RequestParam("studentName") String theName, Model model) {
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+
+		// create the message
+		String result = "Hey My Friend " + theName;
+
+		// add message to the model
+		model.addAttribute("message", result);
+
+		return "helloworld-nameShout";
+	}
+}
